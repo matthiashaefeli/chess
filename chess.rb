ba664ba0d1df07b2e0ea8b50280b8520a1d3410b
matthiashaefeli <haefeli.matthias@gmail.com>
2018-06-23 18:20:15 -0500

    adding tests and classes

--- chess.rb
+++ chess.rb
@@ -1,20 +1,47 @@
-class Pawn
+class Figure
+  attr_reader :moves
+
   def initialize(position)
     @x = position[0]
     @y = position[1]
-
-    @moves = [
-      [1, 0]
-    ]
-
-    def position
-      [@x,@y]
-    end
-
-    def moves
-      @moves
-    end
-
   end
 
+  def position
+    [@x, @y]
+  end
+end
+
+class Pawn < Figure
+  def initialize(position)
+    super
+    @moves = [[1, 0]]
+  end
+end
+
+class Rook < Figure
+  def initialize(position)
+    super
+    @moves = [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1]
+    ]
+  end
+end
+
+class Knight < Figure
+  def initialize(position)
+    super
+    @moves = [
+      [2, 1],
+      [2, -1],
+      [1, 2],
+      [1, -2],
+      [-1, 2],
+      [-1, -2],
+      [-2, 1],
+      [-2, -1]
+    ]
+  end
 end
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,14 +1,14 @@
 require './chess.rb'
 
 describe 'pawn' do
-  let(:pawn){Pawn.new(["a", 2])}
+  let(:pawn) { Pawn.new(['a', 2]) }
 
   it 'creates a instance of a pawn' do
     expect(pawn).to be_instance_of(Pawn)
   end
 
   it 'has a position' do
-    expect(pawn.position).to eq ["a", 2]
+    expect(pawn.position).to eq ['a', 2]
   end
 
   it 'has moves' do
@@ -16,3 +16,34 @@ describe 'pawn' do
   end
 end
 
+describe 'rook' do
+  let(:rook) { Rook.new(['a', 1]) }
+
+  it 'creates a instance of a rook' do
+    expect(rook).to be_instance_of(Rook)
+  end
+
+  it 'has a position' do
+    expect(rook.position).to eq ['a', 1]
+  end
+
+  it 'has moves' do
+    expect(rook.moves).to be_kind_of(Array)
+  end
+end
+
+describe 'knight' do
+  let(:knight) { Knight.new(['b', 1]) }
+
+  it 'creates a instance of a knight' do
+    expect(knight).to be_instance_of(Knight)
+  end
+
+  it 'has a position' do
+    expect(knight.position).to eq ['b', 1]
+  end
+
+  it 'has moves' do
+    expect(knight.moves).to be_kind_of(Array)
+  end
+end
