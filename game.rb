2554b2e547194ba223365a304dfbac04ddb4c0d8
matthiashaefeli <haefeli.matthias@gmail.com>
2018-06-25 16:39:11 -0500

    add method to start game

---
+++ game.rb
@@ -0,0 +1,14 @@
+require './chess.rb'
+
+puts 'Get all possible moves for a chess figure'
+puts 'Please type figure'
+figure = gets.chomp
+figure = figure_exist(figure)
+
+puts 'please type position (ex: 1a)'
+position = gets.chomp
+position = position_exist(position)
+puts "all posible moves with #{figure} from #{position} are:"
+get_moves(figure, convert_position(position)).each do |move|
+  puts move.join('')
+end
